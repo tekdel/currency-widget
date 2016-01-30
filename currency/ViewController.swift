a7d3f12da5ed4845a3755612ed1bbc4b35f25f93
Andrey Dolgov <andrey.dolgov@arcadia.spb.ru>
2016-01-30 15:56:04 +0300

    Initial Commit

---
+++ currency/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  currency
+//
+//  Created by Andrey Dolgov on 1/30/16.
+//  Copyright © 2016 Andrey Dolgov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
